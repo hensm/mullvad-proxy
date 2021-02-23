50a662e0f91c0d07e0002720ce32a40d9a68aa9f
hensm <m@matt.tf>
2021-02-23 04:18:54 +0000

    Use more flexible message type definitions

--- src/messages.ts
+++ src/messages.ts
@@ -4,33 +4,43 @@ import Messenger from "./lib/Messenger";
 import { ConnectionDetails } from "./lib/mullvadApi";
 
 
-export type Messages = [
-    {
-        subject: "popup:/update"
-      , data: {
-            isConnected?: boolean;
-            isConnecting?: boolean;
-            host?: string;
-        }
+type MessagesBase = {
+    "popup:/update": {
+        isConnected?: boolean
+      , isConnecting?: boolean
+      , host?: string
     }
 
-    // background
-  , {
-        subject: "background:/connect"
-      , data: {
-            proxyHost: string;
-            details: ConnectionDetails;
-        }
+  , "background:/connect": {
+        proxyHost: string
+      , details: ConnectionDetails
     }
-  , {
-        subject: "background:/disconnect"
+  , "background:/disconnect": {}
+  , "background:/updateConnectionDetails": {
+        details: ConnectionDetails
     }
-  , {
-        subject: "background:/updateConnectionDetails"
-      , data: {
-            details: ConnectionDetails
-        }
-    }
-];
+}
 
-export default new Messenger<Messages>();
+interface MessageBase<K extends keyof MessagesBase> {
+    subject: K;
+    data: MessagesBase[K];
+}
+
+type Messages = {
+    [K in keyof MessagesBase]: MessageBase<K>;
+}
+
+/**
+ * For better call semantics, make message data key optional if
+ * specified as blank or with all-optional keys.
+ */
+type NarrowedMessage<L extends MessageBase<keyof MessagesBase>> =
+    L extends any
+        ? {} extends L["data"]
+            ? Omit<L, "data"> & Partial<L>
+            : L
+        : never;
+
+export type Message = NarrowedMessage<Messages[keyof Messages]>;
+
+export default new Messenger<Message>();
