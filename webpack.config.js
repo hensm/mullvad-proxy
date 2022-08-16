550ca45b06889b7fb034eab0074457651a604cd2
hensm <m@matt.tf>
2022-08-16 01:53:56 +0100

    Re-format remaining files

--- webpack.config.js
+++ webpack.config.js
@@ -3,67 +3,65 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
-
 const INCLUDE_PATH = path.resolve(__dirname, "src");
 const OUTPUT_PATH = path.resolve(__dirname, "dist");
 
 module.exports = {
     entry: {
-        "background": `${INCLUDE_PATH}/background.ts`
-      , "ui/popup/popup": `${INCLUDE_PATH}/ui/popup/index.tsx`
-      , "ui/options/options": `${INCLUDE_PATH}/ui/options/index.tsx`
-    }
-  , output: {
-        filename: "[name].bundle.js"
-      , path: OUTPUT_PATH
-    }
-  , module: {
+        background: `${INCLUDE_PATH}/background.ts`,
+        "ui/popup/popup": `${INCLUDE_PATH}/ui/popup/index.tsx`,
+        "ui/options/options": `${INCLUDE_PATH}/ui/options/index.tsx`
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: OUTPUT_PATH
+    },
+    module: {
         rules: [
             {
-                test: /\.tsx?$/
-              , resolve: {
-                    extensions: [ ".ts", ".tsx" ]
-                }
-              , include: INCLUDE_PATH
-              , use: "ts-loader"
+                test: /\.tsx?$/,
+                resolve: {
+                    extensions: [".ts", ".tsx"]
+                },
+                include: INCLUDE_PATH,
+                use: "ts-loader"
             }
         ]
-    }
-  , plugins: [
+    },
+    plugins: [
         // Copy static assets
         new CopyPlugin({
             patterns: [
                 {
-                    from: INCLUDE_PATH
-                  , to: OUTPUT_PATH
-                  , globOptions: {
-                        ignore: [ "**.ts", "**.tsx" ]   
+                    from: INCLUDE_PATH,
+                    to: OUTPUT_PATH,
+                    globOptions: {
+                        ignore: ["**.ts", "**.tsx"]
                     }
-                }
-              , {
+                },
+                {
                     from: "./node_modules/webextension-polyfill/dist/browser-polyfill.min.js"
                 }
             ]
         })
-    ]
-  , resolve: {
+    ],
+    resolve: {
         alias: {
-            "react": "preact/compat"
-          , "react-dom": "preact/compat"
+            react: "preact/compat",
+            "react-dom": "preact/compat"
         }
-    }
-  , optimization: {
+    },
+    optimization: {
         splitChunks: {
             cacheGroups: {
                 react: {
-                    test: /[\\/]node_modules[\\/](preact|preact-compat)[\\/]/
-                  , name: "react"
-                  , chunks: "all"
-                  , enforce: true
+                    test: /[\\/]node_modules[\\/](preact|preact-compat)[\\/]/,
+                    name: "react",
+                    chunks: "all",
+                    enforce: true
                 }
             }
         }
-    }
-  , mode: "development"
-  , devtool: "source-map"
+    },
+    mode: "production"
 };
